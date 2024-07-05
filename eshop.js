c2c03fdf78a489cd855298edc2f6e48e15ab6758
joe <johanst232@gmail.com>
2024-07-04 21:31:28 -0300

    adicion

---
+++ eshop.js
@@ -0,0 +1,116 @@
+const btnAumentar1 = document.querySelector('.bt1');
+const btnDisminuir1 = document.querySelector('.bt2');
+const resultado1 = document.querySelector('#resultado1');
+const btnAumentar2 = document.querySelector('.bt3');
+const btnDisminuir2 = document.querySelector('.bt4');
+const resultado2 = document.querySelector('#resultado2');
+const btnAumentar3 = document.querySelector('.bt5');
+const btnDisminuir3 = document.querySelector('.bt6');
+const resultado3 = document.querySelector('#resultado3');
+const btnAumentar4 = document.querySelector('.bt7');
+const btnDisminuir4 = document.querySelector('.bt8');
+const resultado4 = document.querySelector('#resultado4');
+const btnAumentar5 = document.querySelector('.bt9');
+const btnDisminuir5 = document.querySelector('.bt10');
+const resultado5 = document.querySelector('#resultado5');
+const container = document.querySelector('.container');
+const btnCompra = document.querySelector('.compra');
+
+let contador1 = 0;
+let contador2 = 0;
+let contador3 = 0;
+let contador4 = 0;
+let contador5 = 0;
+
+// Boton 1
+btnAumentar1.addEventListener('click', () => {
+    contador1++
+    actualizarContador1();
+});
+
+btnDisminuir1.addEventListener('click', () => {
+if (contador1 > 0) {
+    contador1--
+    actualizarContador1();
+}
+});
+
+const actualizarContador1 = () => {
+    resultado1.textContent = contador1;
+};
+
+// Boton 2
+btnAumentar2.addEventListener('click', () => {
+    contador2++
+    actualizarContador2();
+});
+
+btnDisminuir2.addEventListener('click', () => {
+if (contador2 > 0) {
+    contador2--
+    actualizarContador2();
+}
+});
+
+const actualizarContador2 = () => {
+    resultado2.textContent = contador2;
+};
+
+// Boton 3
+btnAumentar3.addEventListener('click', () => {
+    contador3++
+    actualizarContador3();
+});
+
+btnDisminuir3.addEventListener('click', () => {
+if (contador3 > 0) {
+    contador3--
+    actualizarContador3();
+}
+});
+
+const actualizarContador3 = () => {
+    resultado3.textContent = contador3;
+};
+
+// Boton 4
+btnAumentar4.addEventListener('click', () => {
+    contador4++
+    actualizarContador4();
+});
+
+btnDisminuir4.addEventListener('click', () => {
+if (contador4 > 0) {
+    contador4--
+    actualizarContador4();
+}
+});
+
+const actualizarContador4 = () => {
+    resultado4.textContent = contador4;
+};
+
+// Boton 5
+btnAumentar5.addEventListener('click', () => {
+    contador5++
+    actualizarContador5();
+});
+
+btnDisminuir5.addEventListener('click', () => {
+if (contador5 > 0) {
+    contador5--
+    actualizarContador5();
+}
+});
+
+const actualizarContador5 = () => {
+    resultado5.textContent = contador5;
+};
+
+btnCompra.addEventListener('click', () => {
+    localStorage.setItem('1',JSON.stringify(contador1));
+    localStorage.setItem('2',JSON.stringify(contador2));
+    localStorage.setItem('3',JSON.stringify(contador3));
+    localStorage.setItem('4',JSON.stringify(contador4));
+    localStorage.setItem('5',JSON.stringify(contador5));
+});
---
+++ total.js
@@ -0,0 +1,10 @@
+/* const carrito={}
+const total = document.querySelector('.factura'); */
+
+for (var i = 1; i <= 5; i++) {
+    console.log(localstorage.getItem(i));
+    /* carrito.push(localstorage.getItem(i)) */
+}
+
+
+/* total.textContent = carrito.join */
